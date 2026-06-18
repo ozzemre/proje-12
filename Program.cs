eb87ae645e831ddbf8fcfbfd6ed641ac6dc73d39
Emre Öz <emreadigeee@gmail.com>
2026-06-18 12:38:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace PROJE12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Sayıyı Giriniz : ");
+                string Metin = Console.ReadLine();
+                int Sayi = Convert.ToInt32(Metin);
+
+                bool AsalMi = true;
+
+                for (int i = 2; i < Sayi; i++)
+                {
+                    if (Sayi % i == 0)
+                    {
+                        AsalMi = false;
+                    }
+                }
+
+                if (AsalMi == true)
+                {
+                    Console.WriteLine("{0} sayısı asaldır.", Sayi);
+                }
+                else
+                {
+                    Console.WriteLine("{0} sayısı asal değildir.", Sayi);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HATA : {0}", ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
